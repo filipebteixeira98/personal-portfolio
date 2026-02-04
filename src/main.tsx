431cbdfe0bbc7d4432208604491f9bdf570c6cfa
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-02-04 13:43:15 -0300

    feat(ui): introduce sidebar component

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import { createRoot } from 'react-dom/client';
 import '@/index.css';
 
 import { App } from '@/App';
+import { Sidebar } from '@/components/Sidebar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10'>
+      <Sidebar />
+      <App />
+    </div>
   </StrictMode>,
 );
