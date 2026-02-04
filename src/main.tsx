8376d0e6b5614d61350a83ef14dec1ce98aa22ef
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-02-04 14:11:58 -0300

    feat(ui): add floating menu navigation component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { createRoot } from 'react-dom/client';
 import '@/index.css';
 
 import { App } from '@/App';
+import { FloatingMenu } from '@/components/FloatingMenu';
 import { Sidebar } from '@/components/Sidebar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10'>
       <Sidebar />
+      <FloatingMenu />
       <App />
     </div>
   </StrictMode>,
